106c96dd8888955d5b7d6eb9af3c71d20a9ff6f8
Jhonnatan7br <104907786+Jhonnatan7br@users.noreply.github.com>
2023-11-20 19:46:58 +0100

    Updated

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -5,29 +5,46 @@
 # For example:
 # ./run_pipeline.sh 2020-01-01 2020-01-31 data/raw_data.csv data/processed_data.csv models/model.pkl data/test_data.csv predictions/predictions.json
 
-# Get command line arguments
-start_date="2022-01-01"
-end_date="2023-01-01"
-raw_data_file="$3"
-processed_data_file="$4"
-model_file="$5"
-test_data_file="C:\Users\Jhonnatan\Documents\GitHub\EcoForecast-Schneider_Electric\data\test.csv"
-predictions_file="predictions/example_predictions.json"
+#!/bin/bash
+
+# Set the base directory to the parent directory of this script
+BASE_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; cd .. ; pwd -P )"
+
+# Define paths for data, models, and predictions relative to the base directory
+DATA_DIR="${BASE_DIR}/data"
+MODELS_DIR="${BASE_DIR}/models"
+PREDICTIONS_DIR="${BASE_DIR}/predictions"
+SRC_DIR="${BASE_DIR}/src"
+
+# Command line arguments or default values for file names
+start_date=${1:-"2022-01-01"}
+end_date=${2:-"2023-01-01"}
+raw_data_file="${DATA_DIR}/${3:-raw_data.csv}"
+processed_data_file="${DATA_DIR}/${4:-processed_data.csv}"
+model_file="${MODELS_DIR}/${5:-model.pkl}"
+test_data_file="${DATA_DIR}/test.csv"  # Assuming test.csv is the correct file name
+predictions_file="${PREDICTIONS_DIR}/example_predictions.json"
+
+# Navigate to the script directory
+cd "${SRC_DIR}"
 
 # Run data_ingestion.py
 echo "Starting data ingestion..."
-python src/data_ingestion.py --start_date="$start_date" --end_date="$end_date" --output_file="$raw_data_file"
+python data_ingestion.py --start_time "${start_date}" --end_time "${end_date}" --output_path "${DATA_DIR}"
 
 # Run data_processing.py
 echo "Starting data processing..."
-python src/data_processing.py --input_file="$raw_data_file" --output_file="$processed_data_file"
+python data_processing.py --input_file "${raw_data_file}" --output_file "${processed_data_file}"
 
 # Run model_training.py
 echo "Starting model training..."
-python src/model_training.py --input_file="$processed_data_file" --model_file="$model_file"
+python model_training.py --input_file "${processed_data_file}" --model_file "${model_file}"
 
 # Run model_prediction.py
 echo "Starting prediction..."
-python src/model_prediction.py --input_file="$test_data_file" --model_file="$model_file" --output_file="$predictions_file"
+python model_prediction.py --input_file "${test_data_file}" --model_file "${model_file}" --output_file "${predictions_file}"
+
+# Return to the base directory
+cd "${BASE_DIR}"
 
 echo "Pipeline completed."
